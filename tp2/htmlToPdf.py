1a2d2997be7f1aa0647eb5b5f557fea9314f22aa
diogoalves10 <a79751@alunos.uminho.pt>
2021-06-16 11:48:10 +0100

    almost done , falta adicionar as imagens ao pdf e criar o comando de terminar para gerar

--- tp2/htmlToPdf.py
+++ tp2/htmlToPdf.py
@@ -1,43 +1,59 @@
-import requests as r
 from bs4 import BeautifulSoup as bs
-import page_parser as pp
-import pdfkit as pdf
-from w3lib.html import remove_tags
 
-def getLeftLinks(link) -> []:
-    #cagar nas references e deve resolver o problema
+import page_parser as pp
+import pdfkit as pdfk
+import re
+import requests as r
+
+def getLeftLinks(link) :
+    
     pagIni = r.get(link)
-    link_w3 = "https://www.w3schools.com/"
+    
+    link_w3 = "https://www.w3schools.com"
     lang_link = link.split("https://www.w3schools.com/")[1].split('/')[0]
 
     soup = bs(pagIni.text,'html.parser')
     div =soup.find('div', id="sidenav")
     a_s = div.find_all('a') 
+    
+    for a in a_s :
+        tag = a.find({'href':re.compile(r'/')})
+        if not tag == None:
+            tag.decompose()
 
+   
     links = []
 
     for a in a_s :
-        link=link_w3+lang_link+"/"
-        link += a.get('href')
+        if re.search(r'/',a.get('href')):
+            link = link_w3+a.get('href')
+        else:    
+            link=link_w3+'/'+lang_link+"/"
+            link += a.get('href')
         links.append(link)   
-
+    
     return links
 
 def get_all(link):
     lista = getLeftLinks(link)
     htmls =[]
-    print(lista[75])
+    
     
     for l in lista : 
         htmls.append(pp.getPage(l))
     return htmls    
     
-
-
+def genPDF(link,lang_name):
+    html_s = get_all(link)
+    
+    html_final = ""
+    for html in html_s:
+        html_final += str(html)
+    
+    pdfk.from_string(html_final,lang_name+'.pdf')
 
 def main ():
     link = "https://www.w3schools.com/html/default.asp"
-    pdf.from_string(str(get_all(link)[0]),'teste2.pdf')
-    
+    genPDF(link,'html')
 
 main()       
--- tp2/page_parser.py
+++ tp2/page_parser.py
@@ -1,26 +1,11 @@
 import requests as r
 from bs4 import BeautifulSoup as bs
-import os
 from pathlib import Path
-import shutil
-import re
-
 from requests.api import get
 
 
-def make_dir(language):
-    #make dir
-    path = os.path.join("{language}/")
-    path = Path('{language}')
-    if path.exists() and path.is_dir():
-        shutil.rmtree(path)
-        os.makedirs(path)
-    else:
-        os.makedirs(path)
-
 def clearPage(soup):
     #remove w3-btn
-    print(type(soup))
     btns = soup.find_all("a",class_="w3-btn")
     if btns :
         for btn in btns:
@@ -37,44 +22,20 @@ def clearPage(soup):
     if dipls :
         for dip in dipls:
             dip.decompose()
-
+    #remove input id="searchstring"
+    s_s = soup.find_all("input",id="searchstring")
+    if s_s :
+        for s in s_s:
+            s.decompose()
 
 def getPage(link):
     req = r.get(link)
     soup = bs(req.text,'html.parser')
     final_soup = soup.find("div", {"id":"main"})
 
-    #print(type(final_soup.find_all("a",class_="w3-btn")))
-    if ((final_soup.find_all("a",class_="w3-btn")) != None):
-        
-        btns = final_soup.find_all("a",class_="w3-btn")
-        if btns :
-            for btn in btns:
-                btn.decompose()
-
-    if (final_soup.find_all("form",id="w3-exerciseform")!= None) :   
-        #remove form id="w3-exerciseform"
-        forms = final_soup.find_all("form",id="w3-exerciseform")
-        if forms:
-            for form in forms:
-                form.decompose()
-
-    if (final_soup.find_all("div",id="getdiploma")!= None) :  
-        #remove div id="getdiploma"
-        dipls = final_soup.find_all("div",id="getdiploma")
-        if dipls :
-            for dip in dipls:
-                dip.decompose()
-        
+    clearPage(final_soup)
+    
     return final_soup
    
 
 
-'''
-def main():
-    link = "https://www.w3schools.com/html/html_intro.asp"
-    #print(getPage(link))
-    getPage(link)
-
-main()
-'''
