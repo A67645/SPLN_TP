e9dbb104c2184636687054ed90e7f4c0257438a8
A67645 <antonio.sergio.costa.gomes@gmail.com>
2021-06-18 17:18:42 +0100

    esta a dar erro ContentNotFoundError vindo do pdfkit

--- tp2/htmlToPdf.py
+++ tp2/htmlToPdf.py
@@ -7,59 +7,53 @@ import re
 import requests as r
 
 def getLeftLinks(link) :
-    
+
     pagIni = r.get(link)
-    
+
     link_w3 = "https://www.w3schools.com"
     lang_link = link.split("https://www.w3schools.com/")[1].split('/')[0]
 
     soup = bs(pagIni.text,'html.parser')
     div =soup.find('div', id="sidenav")
-    a_s = div.find_all('a') 
-    
+    a_s = div.find_all('a')
+
     for a in a_s :
         tag = a.find({'href':re.compile(r'/')})
         if not tag == None:
             tag.decompose()
 
-   
+
     links = []
 
     for a in a_s :
         if re.search(r'/',a.get('href')):
             link = link_w3+a.get('href')
-        else:    
+        else:
             link=link_w3+'/'+lang_link+"/"
             link += a.get('href')
-        links.append(link)   
-    
+        links.append(link)
+
     return links
 
 def get_all(link):
     lista = getLeftLinks(link)
     htmls =[]
-    
-    
-    for l in lista : 
+
+
+    for l in lista :
         htmls.append(pp.getPage(l))
-    return htmls    
-    
+    return htmls
+
 def genPDF(link,lang_name):
     html_s = get_all(link)
-    
+
     html_final = ""
     for html in html_s:
         html_final += str(html)
-    
+
     pdfk.from_string(html_final,lang_name+'.pdf')
 
-def main ():
-    link = "https://www.w3schools.com/" + sys.argv[1] + "/default.asp"
-    if sys.argv[1] == 'cs':
-        link = "https://www.w3shcools.com/" + sys.argv[1] + "/index.php"
-    pdf = sys.argv[1]
-    if sys.argv[2] == '-o':
-        pdf = sys.argv[3]
-    genPDF(link, pdf)
-
-main()       
+def main():
+    link = "https://www.w3schools.com/html/default.asp"
+    genPDF(link, 'html')
+main()
---
+++ tp2/w3schools.py
@@ -0,0 +1,99 @@
+import requests as r
+from bs4 import BeautifulSoup as bs
+from pathlib import Path
+from requests.api import get
+import sys
+import pdfkit as pdfk
+import re
+
+def clearPage(soup):
+    #remove w3-btn
+    btns = soup.find_all("a",class_="w3-btn")
+    if btns :
+        for btn in btns:
+            btn.decompose()
+
+    #remove form id="w3-exerciseform"
+    forms = soup.find_all("form",id="w3-exerciseform")
+    if forms:
+        for form in forms:
+            form.decompose()
+
+    #remove div id="getdiploma"
+    dipls = soup.find_all("div",id="getdiploma")
+    if dipls :
+        for dip in dipls:
+            dip.decompose()
+    #remove input id="searchstring"
+    s_s = soup.find_all("input",id="searchstring")
+    if s_s :
+        for s in s_s:
+            s.decompose()
+
+def getPage(link):
+    req = r.get(link)
+    soup = bs(req.text,'html.parser')
+    final_soup = soup.find("div", {"id":"main"})
+
+    clearPage(final_soup)
+
+    return final_soup
+
+def getLeftLinks(link) :
+
+    pagIni = r.get(link)
+
+    link_w3 = "https://www.w3schools.com"
+    lang_link = link.split("https://www.w3schools.com/")[1].split('/')[0]
+
+    soup = bs(pagIni.text,'html.parser')
+    div =soup.find('div', id="sidenav")
+    a_s = div.find_all('a')
+
+    for a in a_s :
+        tag = a.find({'href':re.compile(r'/')})
+        if not tag == None:
+            tag.decompose()
+
+
+    links = []
+
+    for a in a_s :
+        if re.search(r'/',a.get('href')):
+            link = link_w3+a.get('href')
+        else:
+            link=link_w3+'/'+lang_link+"/"
+            link += a.get('href')
+        links.append(link)
+
+    return links
+
+def get_all(link):
+    lista = getLeftLinks(link)
+    htmls =[]
+
+
+    for l in lista :
+        htmls.append(getPage(l))
+    return htmls
+
+def genPDF(link,lang_name):
+    html_s = get_all(link)
+
+    html_final = ""
+    for html in html_s:
+        html_final += str(html)
+
+    pdfk.from_string(html_final,lang_name+'.pdf')
+
+def main ():
+    link = "https://www.w3schools.com/" + sys.argv[1] + "/default.asp"
+    if sys.argv[1] == 'cs':
+        link = "https://www.w3shcools.com/" + sys.argv[1] + "/index.php"
+    pdf = sys.argv[1]
+    if sys.argv[2] == '-o':
+        pdf = sys.argv[3]
+
+    genPDF(link, pdf)
+
+main()
